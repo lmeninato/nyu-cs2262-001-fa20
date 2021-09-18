46e8e554882121d4ab4888018a75e3076e32e6b7
Lorenzo Meninato <lorenzomeninato@gmail.com>
2021-09-18 16:41:24 -0400

    Add time path to flask app

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import time
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+@app.route('/time')
+def get_time():
+    # from https://stackoverflow.com/a/40359683
+    curr_time = time.strftime('%A %B, %d %Y %H:%M:%S')
+    return "The current time is {}".format(curr_time)
 
 
 app.run(host='0.0.0.0',
